18377ecf20b27175c995a09263c6848d8a649452
Franz Brauße <franz.brausse@manchester.ac.uk>
2021-12-20 07:04:32 +0100

    [cheri] WIP: intro ansi_c.capability_width() + --cheri {hybrid,purecap} + --cheri-uncompressed

--- src/c2goto/cprover_library.cpp
+++ src/c2goto/cprover_library.cpp
@@ -37,12 +37,12 @@ extern "C"
 
 namespace
 {
-/* [cheri ? 1 : 0][floatbv ? 1 : 0][wordsz == 64 ? 1 : 0] */
+/* [cheri][floatbv ? 1 : 0][wordsz == 64 ? 1 : 0] */
 static const struct buffer
 {
   const uint8_t *start;
   size_t size;
-} clibs[2][2][2] = {
+} clibs[3][2][2] = {
   {
     {
       {&clib32_buf[0], clib32_buf_size},
